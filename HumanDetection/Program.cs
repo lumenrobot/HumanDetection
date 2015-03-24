9fee9f7eda12e9e6713dadf787a15df2e57e03b3
Sigit Ari Wijanarko <sigit_saw@ymail.com>
2015-03-24 15:10:19 +0700

    test commit

--- HumanDetection/Program.cs
+++ HumanDetection/Program.cs
@@ -8,7 +8,7 @@ namespace HumanDetection
     static class Program
     {
         /// <summary>
-        /// The main entry point for the application.
+        /// The main entry point for the application. 
         /// </summary>
         [STAThread]
         static void Main()
